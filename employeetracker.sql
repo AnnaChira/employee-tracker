5ee9e37b8d8615c12c35009c11c78624ab7f6060
Anna Chirapongse <achirapongse@gmail.com>
2020-11-15 19:52:53 -0500

    Updates to the schema

--- employeetracker.sql
+++ employeetracker.sql
@@ -3,6 +3,29 @@ CREATE database employee_tracker;
 
 USE employee_tracker;
 
-CREATE TABLE employee_tracker (
-    
-)
+CREATE TABLE employee (
+  position INT NOT NULL,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,  
+  PRIMARY KEY (position)
+);
+
+CREATE TABLE roles (
+  position INT NOT NULL,
+  title VARCHAR(30) NULL,
+  salary decimal,
+  department_id INT NULL,  
+  PRIMARY KEY (position)
+);
+
+CREATE TABLE departments (
+  position INT NOT NULL,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (position)
+);
+
+SELECT * FROM employee;
+SELECT * FROM roles;
+SELECT * FROM departments;
